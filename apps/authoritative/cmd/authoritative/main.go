574c9564fa4e3e045a29b6b2058f7d8f1e08dbfa
georgengyn <g.nguyen@wustl.edu>
2025-10-21 20:27:04 -0500

    server receives order requests

--- apps/authoritative/cmd/authoritative/main.go
+++ apps/authoritative/cmd/authoritative/main.go
@@ -7,32 +7,33 @@ import (
 	"log"
 	"net"
 
-	"github.com/jaximus808/delivery-gdg-platform/main/apps/authoritative/example/proto"
+	pb "github.com/jaximus808/delivery-gdg-platform/main/apps/authoritative/proto"
 	"google.golang.org/grpc"
 )
 
 type server struct {
-	proto.UnimplementedHelloServiceServer
+	pb.UnimplementedOrderHandlerServer
 }
 
-func (s *server) SayHello(ctx context.Context, req *proto.HelloRequest) (*proto.HelloResponse, error) {
-	return &proto.HelloResponse{
-		Message: fmt.Sprintf("Hello, %s!", req.GetName()),
+func (s *server) PlaceOrder(ctx context.Context, order *pb.Order) (*pb.PlaceOrderResponse, error) {
+	fmt.Print("Received an order! \n")
+
+	return &pb.PlaceOrderResponse{
+		OrderId: order.OrderId,
+		Message: fmt.Sprintf("Received order %d from %s! They want %s from %s.",
+			order.OrderId, order.CustomerName, order.Item, order.VendorName),
 	}, nil
-}
-
-func add(x int, y int) int {
-	return x + y
 }
 
 func main() {
 	lis, err := net.Listen("tcp", ":50051")
+
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
 	s := grpc.NewServer()
-	proto.RegisterHelloServiceServer(s, &server{})
+	pb.RegisterOrderHandlerServer(s, &server{})
 
 	log.Println("gRPC server listening on :50051")
 	if err := s.Serve(lis); err != nil {
